a2c0c1fe0ee491a81ba79078dafcfd72bf49116b
1Aditya7 <aditya17parthiban@gmail.com>
2024-07-01 14:39:51 +0530

    -m Import the Project

---
+++ alpaca_config/keys.py
@@ -0,0 +1,7 @@
+config = {
+    'key_id': 'AKNI4GGNXZRXGV37TOVW',
+    'secret_key': 'R9hOTTxYqdAvm1hscoYqhS8ME6LR9L2zp8xElDR1',
+    'redpanda_brokers': 'localhost:9092, localhost:9093',
+    'base_url': 'https://data.alpaca.markets/v1beta1/',
+    'trade_api_base_url': 'https://api.alpaca.markets/v2'
+}
